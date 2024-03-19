b69acbde43c25e7a08249180ca8c090980b1c2e1
YuriShevchuk <yurii.shev4uk@gmail.com>
2024-03-19 12:02:06 +0200

    [Geniusee] - Delete js file

--- index.js
+++
@@ -1,9 +0,0 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     // Initialize Parsley on your form
-//     $('#checkoutForm').parsley();
-
-//     // Add Parsley validation for each form field on blur
-//     $('#checkoutForm input, #checkoutForm select, #checkoutForm textarea').on('blur', function () {
-//         $('#checkoutForm').parsley().validate();
-//     });
-// });
